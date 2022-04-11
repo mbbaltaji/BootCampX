9bca84b8d1f8835c552293ac1e401f74eca86a91
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-04-11 19:25:32 +0000

    add GROUP BY and JOIN queries

---
+++ 3_queries_group_by/6_average_completion_time.sql
@@ -0,0 +1,7 @@
+SELECT students.name AS student, 
+AVG(assignment_submissions.duration) AS avg_assign_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY avg_assign_duration DESC;
